492516c62ad156cbc848c56e450ca6eddad3708d
prandrea <prandrea@utexas.edu>
2022-05-20 16:32:27 -0500

    added HTML and start button to js

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+
+
+//Button starts quiz
+var startbutton = document.querySelector("#start-quiz");
+
+startbutton.addEventListener("click", function() {}
+
+
+//Same button should start timer
+
+//timer should be displayed throughout quiz
+
+
+
+// Quiz questions begin, and answers are stored
+
+
+// If correct points are added
+//If incorrect time is deducted
+
+// Quiz ends if timer reaches 0 or if questions run out
+
+//User enters initials to save score
+//Theres a place for all high scores to be displayed
+
+
+
+
+
+
+
+
+
+
+
+
+//Questions
+//1. Commonly used data types do NOT include__________.
+//strings, booleans, *alerts, numbers
+//2. Inside which HTML element do you put the JavaScript?
+// <js> <title> *<script> <javascript>
+//3. How do you add a comment in JavaScript?
+// *//comment <!--comment--!> 
+//4. The condition in an if/else statement is inclosed in _________.
+// curly brackets, *parenthesis, quotes, square brackets
+//5. How do you round the number 4.15 to the nearest integer?
+// round(4.15) int.round(4.15) *Math.round(4.15) Math.int(4.15)
