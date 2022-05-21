3417fc526d6d0a06329d7f5c5d7c76524d02619f
prandrea <prandrea@utexas.edu>
2022-05-21 14:47:21 -0500

    setInterval, clearInterval for timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,26 +11,53 @@
 // THEN I can save my initials and score
 
 
+//control css using Javascript: hide questions and scoreboard initially and display questions when you start 
+var timer = 5
 
 //Button starts quiz
 var startbutton = document.querySelector("#start-quiz");
-
-startbutton.addEventListener("click", function() {}
-
-
+var startFunction = function() {
+//Display questions-store questions as variable, array or object
+//Display answer choices
+//add listener events to each response
 //Same button should start timer
 
-//timer should be displayed throughout quiz
+var timeInterval = setInterval(function () {timer--, console.log(timer)
+if (timer === 0){
+    clearInterval(timeInterval);
+    //end quiz (hide remaining questions) because they're out of time 
+    endquiz()
+}
+}, 1000);
 
+console.log("Andrea");
+
+}
+function endquiz() {
+    console.log(timer)
+    var score = timer
+}
+
+// Quiz ends if timer reaches 0 or if questions run out
+// if questions run out then store current timer as score and end quiz
+
+
+startbutton.addEventListener("click", startFunction);
+
+
+
+
+//timer should be displayed throughout quiz
+//create html element to display timer variable
 
 
 // Quiz questions begin, and answers are stored
 
 
-// If correct points are added
-//If incorrect time is deducted
 
-// Quiz ends if timer reaches 0 or if questions run out
+//If incorrect, time is deducted
+
+
 
 //User enters initials to save score
 //Theres a place for all high scores to be displayed
